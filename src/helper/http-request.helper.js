6c04851d7af3f0384b4a6f8b05f748e699c16a19
Nam Phan <sockyone>
2019-07-17 20:05:18 +0700

    login

---
+++ src/helper/http-request.helper.js
@@ -0,0 +1,6 @@
+import UserService from './../service/user.service';
+import axios from 'axios';
+
+function get(url) {
+
+} 
---
+++ src/service/user.service.js
@@ -0,0 +1,11 @@
+class UserService {
+    constructor() {
+        this.user = null;
+        this.token = null;
+    }
+}
+
+
+let userService = new UserService();
+
+export default userService;
